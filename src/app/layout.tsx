898f2eff16cf25dd1051eca710ecda3ca6195940
Emon <emonhasan7650@gmail.com>
2024-05-22 19:03:24 +0600

    common layout some page added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
-import Providers from "./lib/providers/Provider";
+import Providers from "@/lib/providers/Provider";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
