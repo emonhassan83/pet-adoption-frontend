a08cadcc08ff097fadf286a45aa7ea86cb98a3e8
Emon <emonhasan7650@gmail.com>
2024-05-27 03:46:29 +0600

    some templete page added

--- src/app/(withDashboardLayout)/dashboard/user/add-adoption/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/user/add-adoption/page.tsx
@@ -1,9 +1,121 @@
+"use client";
+
+import PetFrom from "@/components/Forms/PetForm";
+import PetInput from "@/components/Forms/PetInput";
+import { Box, Button, Grid, Typography } from "@mui/material";
+
 const AddAdoptionPage = () => {
-    return (
-        <div>
-            Add adoption page
-        </div>
-    );
+  const handleSubmit = () => {};
+
+  return (
+    <Box sx={{textAlign: "center"}}>
+      <Typography variant="h6" color="primary" mt={3}>
+        Apply to Adopt
+      </Typography>
+      <Box>
+        <Typography variant="body2" color="secondary">
+          Please note you will not be able to submit your application until all
+          fields marked as REQUIRED have been entered.
+        </Typography>
+      </Box>
+      <PetFrom
+        onSubmit={handleSubmit}
+        //   resolver={zodResolver(validationSchema)}
+        // defaultValues={{
+        //   email: "",
+        //   password: "",
+        // }}
+      >
+        <Grid container spacing={2} my={1}>
+          <Grid item xs={12} sm={6}>
+            <PetInput name="name" label="Name" type="text" fullWidth={true} />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <PetInput
+              name="email"
+              label="Email"
+              type="email"
+              fullWidth={true}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <PetInput
+              name="address"
+              label="Address"
+              type="text"
+              fullWidth={true}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <PetInput
+              name="contractNo"
+              label="Contract No"
+              type="tel"
+              fullWidth={true}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <PetInput
+              name="petsNeutered"
+              label="Are other pets neutered?"
+              type="text"
+              fullWidth={true}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <PetInput
+              name="secureOutdoorArea"
+              label="Do you have a secure outdoor area?"
+              type="text"
+              fullWidth={true}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <PetInput
+              name="animalSleep"
+              label="Where will the animal sleep at night?"
+              type="text"
+              fullWidth={true}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <PetInput
+              name="animalAlonePeriodsTime"
+              label="Will the animal be left alone for long periods of time?"
+              type="text"
+              fullWidth={true}
+            />
+          </Grid>
+          <Grid item sm={12}>
+            <PetInput
+              name="petOwnershipExperience"
+              label="Do you have any pet ownership experience?"
+              type="text"
+              fullWidth={true}
+            />
+          </Grid>
+          <Grid item sm={12}>
+            <PetInput
+              name="detailsSupport"
+              label="Any other details to support your application?"
+              type="text"
+              fullWidth={true}
+            />
+          </Grid>
+        </Grid>
+
+        <Button
+          fullWidth={true}
+          sx={{
+            margin: "10px 0",
+          }}
+          type="submit"
+        >
+          Adoption Request
+        </Button>
+      </PetFrom>
+    </Box>
+  );
 };
 
 export default AddAdoptionPage;
--- src/app/(withDashboardLayout)/dashboard/user/manage-adoption/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/user/manage-adoption/page.tsx
@@ -1,9 +1,90 @@
+"use client";
+
+import { useGetMyAdoptionRequestsQuery } from "@/redux/api/adoptionApi";
+import { Box, IconButton } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import Image from "next/image";
+import DeleteIcon from "@mui/icons-material/Delete";
+
 const ManageAdoptionPage = () => {
+    const { data, isLoading } = useGetMyAdoptionRequestsQuery({});
+    console.log(data);
+  
+    const columns: GridColDef[] = [
+      {
+          field: "user.name",
+          headerName: "Req. User",
+          flex: 1,
+          renderCell: ({ row }) => {
+            return <Box>{row.user ? row.user.name : "Unknown"}</Box>;
+          },
+        },
+      {
+          field: "user.email",
+          headerName: "User Email",
+          flex: 1,
+          renderCell: ({ row }) => {
+            return <Box>{row.user ? row.user.email : "Unknown"}</Box>;
+          },
+        },
+      {
+          field: "pet.name",
+          headerName: "Pet Name",
+          flex: 1,
+          renderCell: ({ row }) => {
+            return <Box>{row.pet ? row.pet.name : "Unknown"}</Box>;
+          },
+        },
+      {
+        field: "image",
+        headerName: "Pet Image",
+        flex: 1,
+        renderCell: ({ row }) => {
+          return (
+            <Box>
+              <Image src={row.pet.image} width={40} height={40} alt="Pet Image" />
+            </Box>
+          );
+        },
+      },
+      { field: "animalSleep", headerName: "Animal Sleep", flex: 1 },
+      { field: "status", headerName: "Status", flex: 1 },
+      {
+        field: "action",
+        headerName: "Action",
+        flex: 1,
+        headerAlign: "center",
+        align: "center",
+        renderCell: ({ row }) => {
+          return (
+            <IconButton color="primary" aria-label="delete">
+              <DeleteIcon />
+            </IconButton>
+          );
+        },
+      },
+    ];
+  
     return (
-        <div>
-            Manage Adoption Page
-        </div>
+      <Box>
+        {!isLoading ? (
+          <Box my={2}>
+            <DataGrid
+              rows={data ?? []}
+              columns={columns}
+              initialState={{
+                pagination: {
+                  paginationModel: { page: 0, pageSize: 5 },
+                },
+              }}
+              pageSizeOptions={[5, 10]}
+            />
+          </Box>
+        ) : (
+          <h1>Loading.....</h1>
+        )}
+      </Box>
     );
-};
+  };
 
 export default ManageAdoptionPage;
