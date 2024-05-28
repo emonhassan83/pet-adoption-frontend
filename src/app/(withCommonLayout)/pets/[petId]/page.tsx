dd3dbf4471257346b15dd0c7573253fa9d8db865
Emon <emonhasan7650@gmail.com>
2024-05-28 12:51:57 +0600

    pet details page added

--- src/app/(withCommonLayout)/pets/[petId]/page.tsx
+++ src/app/(withCommonLayout)/pets/[petId]/page.tsx
@@ -1,9 +1,128 @@
-const PetDetailsPage = () => {
-    return (
-        <div>
-            pet details page
-        </div>
-    );
+import { Box, Button, Container, Typography, Stack } from "@mui/material";
+import Image from "next/image";
+
+const PetDetail = ({ label, value }: { label: string; value: string }) => (
+  <Box
+    sx={{
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+    }}
+  >
+    <Typography>{label}</Typography>
+    <Typography>{value}</Typography>
+  </Box>
+);
+
+const Section = ({ title, children }: { title: string; children: any }) => (
+  <Box sx={{ mt: 4 }}>
+    <Typography textTransform="uppercase" variant="h6" mb={2}>
+      {title}
+    </Typography>
+    {children}
+  </Box>
+);
+
+const PetDetailsPage = async ({ params }: { params: any }) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/pets/${params?.petId}`
+  );
+  const { data: pet } = await res.json();
+  // console.log(pet);
+
+  return (
+    <Container maxWidth="lg" sx={{ my: 4 }}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: 6,
+        }}
+      >
+        <Box sx={{ width: "65%" }}>
+          <Image src={pet.image} height={600} width={600} alt="Pet Image" />
+        </Box>
+        <Box sx={{ width: "40%" }}>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              gap: 4,
+            }}
+          >
+            <Typography variant="h4">{pet?.name}</Typography>
+            <Box
+              sx={{
+                color: "green",
+                padding: "4px 10px",
+                borderRadius: "10px",
+                background: "secondary",
+              }}
+            >
+              <Typography>Available to adopt</Typography>
+            </Box>
+          </Box>
+          <Typography textTransform="uppercase" mt={2} mb={1}>
+            About
+          </Typography>
+          <Stack direction="column" gap={0.5}>
+            <PetDetail label="Breed" value={pet?.breed} />
+            <PetDetail label="Color" value={pet?.color} />
+            <PetDetail label="Age" value={`${pet?.age} years old`} />
+            <PetDetail label="Gender" value={pet?.gender} />
+            <PetDetail label="Arrived Date" value={pet?.createdAt} />
+            <PetDetail label="Arrived From" value={pet?.user?.address} />
+            <PetDetail label="Size" value={pet?.size} />
+            <PetDetail label="Location" value={pet?.location} />
+            <PetDetail label="Rehoming Fee" value={"$180"} />
+          </Stack>
+          <Button fullWidth sx={{ mt: 2 }}>
+            Apply to Adopt
+          </Button>
+        </Box>
+      </Box>
+
+      <Section title="Description">
+        <Typography>{pet?.description}</Typography>
+      </Section>
+
+      <Section title="Favourite Things">
+        <Box sx={{ display: "flex", gap: 2 }}>
+          <Typography>Belly Rubs</Typography>
+          <Typography>Lead Walks</Typography>
+          <Typography>Playing with Toys</Typography>
+          <Typography>Cuddles on Sofa</Typography>
+        </Box>
+      </Section>
+
+      <Section title="Home Requirements">
+        <Box sx={{ display: "flex", gap: 4 }}>
+          <Box>
+            <Typography>Minimum age of children</Typography>
+            <Typography>8</Typography>
+          </Box>
+          <Box>
+            <Typography>Can they live with dogs?</Typography>
+            <Typography>Can live with a dog</Typography>
+          </Box>
+          <Box>
+            <Typography>Can they live with cats?</Typography>
+            <Typography>Cannot live with cats</Typography>
+          </Box>
+        </Box>
+      </Section>
+
+      <Section title="Medical History">
+        <Typography>{pet?.medicalHistory}</Typography>
+      </Section>
+
+      <Section title="Adoption Requirements">
+        <Typography>{pet?.adoptionRequirements}</Typography>
+      </Section>
+    </Container>
+  );
 };
 
 export default PetDetailsPage;
--- src/components/UI/HomePage/OurSolutionSection/OurSolutionSection.tsx
+++ src/components/UI/HomePage/OurSolutionSection/OurSolutionSection.tsx
@@ -1,13 +1,70 @@
-import { Box, Container, Typography } from "@mui/material";
+import assets from "@/assets";
+import { Box, Container, Grid, Typography } from "@mui/material";
+import Image from "next/image";
+
+const cardData = [
+  {
+    image: assets.images.sol_card_img1,
+    title: "Petco Love Adopt",
+    sub_title: "Creating families",
+    description:
+      "Adopt a pet. Grow your family. Fill your home with love. With Petco Love pet adoption, it’s easy to find your match.",
+  },
+  {
+    image: assets.images.sol_card_img2,
+    title: "Petco Love Lost",
+    sub_title: "Reuniting pets",
+    description:
+      "Help reunite lost and found pets with their people by snapping a photo and uploading it on Petco Love Lost, or register your pet in case they ever go missing. It’s free!",
+  },
+  {
+    image: assets.images.sol_card_img3,
+    title: "Petco Love Care",
+    sub_title: "Championing health",
+    description:
+      "Explore accessible veterinary care, free pet vaccines, and pet cancer resources. Because every pet’s health is priceless.",
+  },
+];
 
 const OurSolutionSection = () => {
-    return (
-        <Container>
-            <Box>
-                <Typography>Our Solution</Typography>
-            </Box>
-        </Container>
-    );
+  return (
+    <Container sx={{
+        my: 16
+    }}>
+      <Box textAlign="center" >
+        <Typography variant="h4" component="h1" fontWeight={700}>Our Solution</Typography>
+        <Typography component="p" fontSize={18} fontWeight={400}>
+        Unleashing the power of Love, together.
+        </Typography>
+      </Box>
+
+      <Box sx={{ margin: "30px auto" }}>
+        <Grid container spacing={2}>
+          {cardData?.map((data: any, index: number) => (
+            <Grid item key={index} md={4}>
+              <Box>
+                <Image src={data?.image} alt="data" width={500} height={100} />
+              </Box>
+
+              <Box sx={{
+                width: "500px"
+              }}>
+              <Typography variant="body2" color="text.secondary">
+                {data.sub_title}
+              </Typography>
+              <Typography gutterBottom variant="h5" component="div">
+                {data?.title}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {data.description}
+              </Typography>
+              </Box>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </Container>
+  );
 };
 
 export default OurSolutionSection;
