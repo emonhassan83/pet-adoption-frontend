a734265e921329635c1d441cf5e6f88fd05fa4cf
Emon <emonhasan7650@gmail.com>
2024-08-14 18:02:01 +0600

    our approach section added and donate section modified

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,6 +1,7 @@
 import DonateSection from "@/components/UI/HomePage/DonateSection/DonateSection";
 import HeroSection from "@/components/UI/HomePage/HeroSection/HeroSection";
 import LearnAboutUs from "@/components/UI/HomePage/LearnAboutUs/LearnAboutUs";
+import OurApproachSection from "@/components/UI/HomePage/OurApproach/OurApproach";
 import OurSolutionSection from "@/components/UI/HomePage/OurSolutionSection/OurSolutionSection";
 import PetAdoptSection from "@/components/UI/HomePage/PetAdoptSection/PetAdoptSection";
 import PlanningAdopt from "@/components/UI/HomePage/PlanningAdopt/PlanningAdopt";
@@ -13,7 +14,7 @@ const HomePage = () => {
       <PetAdoptSection/>
       <PlanningAdopt/>
       <OurSolutionSection/>
-      
+      <OurApproachSection/>
       <LearnAboutUs/>
       <DonateSection/>
     </>
---
+++ src/components/UI/HomePage/OurApproach/OurApproach.tsx
@@ -0,0 +1,60 @@
+import assets from "@/assets";
+import { Box, Container, Typography } from "@mui/material";
+import Image from "next/image";
+
+const OurApproachSection = () => {
+  return (
+    <Container
+      sx={{
+        my: 16,
+      }}
+    >
+      <Box textAlign="center" mb={8}>
+        <Typography variant="h4" component="h1" fontWeight={700}>
+          Our Approach
+        </Typography>
+      </Box>
+
+      <Box>
+        <Box
+          sx={{
+            width: "40%",
+            height: "340px",
+            overflow: "hidden",
+            position: "relative",
+            "&:hover img": {
+              transform: "scale(1.1)",
+            },
+          }}
+        >
+          <Image
+            src={assets.images.donate_banner}
+            width={400}
+            height={300}
+            alt="Donate Banner"
+            style={{
+              transition: "transform 0.5s ease",
+              width: "100%",
+              height: "auto",
+              borderRadius: "5px",
+            }}
+          />
+        </Box>
+        <Typography component="p" fontSize={18} fontWeight={400}>
+          Help Us Save Lives: Support Our Mission
+        </Typography>
+        <Typography variant="body2" fontSize={18} fontWeight={400}>
+          Since early 2022, UWARF has successfully completed four critical
+          missions in Ukraine, providing vital care to over 5,500 animals. As we
+          look ahead to 2023 and beyond, we are committed to scaling our efforts
+          with the ambitious goal of sterilizing 400,000 dogs and cats by 2025.
+          At just $44 per animal, your generous support is essential to help us
+          reach this milestone and continue saving lives. Together, we can make
+          a profound difference.
+        </Typography>
+      </Box>
+    </Container>
+  );
+};
+
+export default OurApproachSection;
